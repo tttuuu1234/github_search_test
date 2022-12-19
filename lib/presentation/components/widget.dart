b30feadcef124dd9eb442ce8b15bafc393b27c74
tttuuu1234 <yidongyi8@gmail.com>
2022-12-19 21:14:00 +0900

    #5 検索TextFiledのデザイン調整

---
+++ lib/presentation/components/widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+/// 検索TextFiled
+class SearchTextFiled extends StatelessWidget {
+  const SearchTextFiled({
+    Key? key,
+    required this.keywordController,
+  }) : super(key: key);
+
+  final TextEditingController keywordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: keywordController,
+      textInputAction: TextInputAction.search,
+      decoration: InputDecoration(
+        fillColor: Colors.grey.shade200,
+        filled: true,
+        hintText: 'キーワードを検索',
+        contentPadding: EdgeInsets.zero,
+        prefixIcon: const Icon(Icons.search),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.transparent,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.transparent,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
+      ),
+      onChanged: (value) {},
+    );
+  }
+}
--- lib/presentation/pages/home/page.dart
+++ lib/presentation/pages/home/page.dart
@@ -1,85 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../components/widget.dart';
 import 'notifier.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  late final TextEditingController keywordController;
+
+  @override
+  void initState() {
+    keywordController = TextEditingController(text: '');
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final fetchGitHubRepositoryList = ref.watch(gitHubRespositoryListProvider);
 
     return Scaffold(
+      backgroundColor: Colors.white,
       body: fetchGitHubRepositoryList.when(
         data: (data) {
-          return CustomScrollView(
-            slivers: [
-              const SliverAppBar(
-                title: Text('リポジトリ'),
-              ),
-              SliverPadding(
-                padding: const EdgeInsets.all(8),
-                sliver: SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    childCount: data.list.length,
-                    (context, index) {
-                      final item = data.list[index];
-                      item.owner.avatarUrl;
+          return Container(
+            margin: const EdgeInsets.only(top: 10),
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  title: SizedBox(
+                    height: 50,
+                    child: SearchTextFiled(
+                      keywordController: keywordController,
+                    ),
+                  ),
+                  backgroundColor: Colors.white,
+                ),
+                SliverPadding(
+                  padding: const EdgeInsets.all(8),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      childCount: data.list.length,
+                      (context, index) {
+                        final item = data.list[index];
+                        item.owner.avatarUrl;
 
-                      return Container(
-                        margin: const EdgeInsets.only(bottom: 8),
-                        child: ListTile(
-                          title: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                children: [
-                                  _AvatarImageArea(
-                                    avaterUrl: item.owner.avatarUrl,
-                                  ),
-                                  const SizedBox(width: 8),
-                                  Text(item.owner.name),
-                                ],
-                              ),
-                              const SizedBox(height: 8),
-                              Text(
-                                item.name,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(height: 8),
-                              Text(item.description),
-                              const SizedBox(height: 8),
-                              Row(
-                                children: [
-                                  const Icon(Icons.star_border),
-                                  Flexible(
-                                    child: Text(
-                                      item.startCount.toString(),
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 8),
+                          child: ListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    _AvatarImageArea(
+                                      avaterUrl: item.owner.avatarUrl,
                                     ),
+                                    const SizedBox(width: 8),
+                                    Text(item.owner.name),
+                                  ],
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  item.name,
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                  const SizedBox(width: 16),
-                                  const Icon(Icons.circle_sharp),
-                                  Flexible(child: Text(item.language)),
-                                ],
-                              ),
-                            ],
-                          ),
-                          shape: Border(
+                                ),
+                                const SizedBox(height: 8),
+                                Text(item.description),
+                                const SizedBox(height: 8),
+                                Row(
+                                  children: [
+                                    const Icon(Icons.star_border),
+                                    Flexible(
+                                      child: Text(
+                                        item.startCount.toString(),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 16),
+                                    const Icon(Icons.circle_sharp),
+                                    Flexible(child: Text(item.language)),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            shape: Border(
                               bottom: BorderSide(
-                            color: Colors.grey.shade300,
-                          )),
-                          onTap: () {
-                            print('詳細画面遷移');
-                          },
-                        ),
-                      );
-                    },
+                                color: Colors.grey.shade300,
+                              ),
+                            ),
+                            onTap: () {
+                              print('詳細画面遷移');
+                            },
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
-              )
-            ],
+              ],
+            ),
           );
         },
         error: (error, stackTrace) => Center(child: Text(error.toString())),
@@ -88,7 +113,6 @@ class HomePage extends ConsumerWidget {
     );
   }
 }
-
 class _AvatarImageArea extends StatelessWidget {
   const _AvatarImageArea({
     Key? key,
